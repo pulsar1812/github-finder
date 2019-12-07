35c2eb7f00ef3a87facc8782030be4e9d6140458
Patrick Lee <cluster000@gmail.com>
2019-12-07 21:49:13 +0800

    Finished search feature, added User and Alert components

---
+++ src/components/users/User.js
@@ -0,0 +1,54 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import Spinner from '../layout/Spinner';
+
+class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    loading: PropTypes.bool
+  };
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+      </Fragment>
+    );
+  }
+}
+
+export default User;
